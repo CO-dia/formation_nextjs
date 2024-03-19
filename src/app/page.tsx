76d175a203a63e2fab30ff47b0a2f9bf6cb1725f
Dia <rdiadama@gmail.com>
2024-03-19 02:09:13 -0400

    Testing serverside props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import ResourceHighlight from "@/components/ResourceHighlight";
 import { resources } from "../../api/data";
+import { GetServerSideProps } from "next";
 
-export default function Home() {
-
+const Home = ({repo}) => {
   return (
     <main>   
       <ResourceHighlight resources={resources}/>
     </main>
   );
 }
+
+/* export const getServerSideProps = (async () => {
+  // Fetch data from external API
+  const res = await fetch('http://localhost:3000/api/hello')
+  const repo = await res.json()
+  // Pass data to the page via props
+  return { props: { repo } }
+}) satisfies GetServerSideProps<{ repo }> */
+
+export default Home;
